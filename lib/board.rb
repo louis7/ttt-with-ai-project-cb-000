6be6a9a61137ffd30c5464b3ac82d8d4bcc0a773
louis baptiste <louisvis@ymail.com>
2018-10-04 00:08:43 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -6,10 +6,13 @@ def initialize
 end
 
 
-def display
-    @cells
-
-end
+  def display
+   puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+   puts "-----------"
+   puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+   puts "-----------"
+   puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+ end
 
 def reset!
     @cells = [" "," "," "," "," "," "," "," "," "]
